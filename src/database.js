a22bb4e766a4f6633e6fc79d56f022e78c451436
Catalina Krenz <caty_kr_enz@hotmail.com>
2024-02-29 21:53:46 -0300

    ultimo cambio

--- src/database.js
+++ src/database.js
@@ -9,7 +9,7 @@ const mongoose = require("mongoose");
 
 
 mongoose
-  .connect("mongodb+srv://catalinakrenz3316:coderhouse@cluster0.w8yg92g.mongodb.net/Tienda?retryWrites=true&w=majority&appName=Cluster0 ")
+  .connect("mongodb+srv://catalinakrenz3316:coderhouse@cluster0.w8yg92g.mongodb.net/Ecommerce?retryWrites=true&w=majority&appName=Cluster0")
   .then(() => console.log("Conexion exitosa"))
   .catch(() => console.log("Error en la conexion"));
 
